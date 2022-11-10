0b9f250d58fce2c9f3d40e685167d8c37d51b831
yoshioka <y.yoshioka.sys22@morijyobi.ac.jp>
2022-11-10 14:59:03 +0900

    Main.javaの編集

--- Main.java
+++ Main.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
-		System.out.print("身長");
-		double height = s.nextDouble();
+		for(int i=0;i<2;i++){
+		    	System.out.print("身長");
+			double height = s.nextDouble();
 
-		System.out.print("体重");
-		double weight = s.nextDouble();
+			System.out.print("体重");
+			double weight = s.nextDouble();
 
-		double BMI = weight / (height * height);
-		System.out.println("BMI" + String.format("%.2f",BMI));
+			double BMI = weight / (height * height);
+			System.out.println("BMI" + String.format("%.2f",BMI));
+		}
 	}
 }
